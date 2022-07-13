d3229671337b64555e0edc335c7b62bdb29cf42c
David Chang <zdy004007@126.com>
2022-07-13 16:19:31 +0800

    ver Jul13th
    updated bijection marks in accept-url.note
    generated url-x_requested_with.list

--- apps/wikihow/mitmscripts/extract_urls.py
+++ apps/wikihow/mitmscripts/extract_urls.py
@@ -2,27 +2,9 @@ from mitmproxy import http
 from mitmproxy import addonmanager
 from mitmproxy import io
 
-#import io
+import re
 
-#class URLDumper:
-    #def load(self, loader: addonmanager.Loader):
-        ##  method `load` {{{ # 
-        #self.filename: str = "wikihow-urls.list"
-        #self.file: io.TextIOWrapper = open(self.filename, "w")
-        ##  }}} method `load` # 
-#
-    #def response(self, flow: http.HTTPFlow):
-        #if flow.host=="www.wikihow.com":
-            #self.file.write(flow.url)
-#
-    #def done(self):
-        #self.file.flush()
-        #self.file.close()
-#
-#addons = [
-        #URLDumper()
-    #]
-
+#  Data Structure Definition {{{ # 
 attributes = [
         "upgrade-insecure-requests",
         "user-agent",
@@ -32,7 +14,7 @@ attributes = [
         "sec-fetch-mode",
         "sec-fetch-user",
         "sec-fetch-dest",
-        "sec-fetch-site"
+        "sec-fetch-site",
         "accept-encoding",
         "accept-language",
         "cookie",
@@ -47,8 +29,25 @@ new_attributes = set()
 
 status_codes = set()
 
-grouped_by_accepts = {}
+url_categories = [
+        "[[:others:]]",
+        "/x/*",
+        "/ev/*",
+        "R /load.php?.\+$\(only=styles\)\@!",
+        "/video/*",
+        "/Special:SherlockController",
+        "/Special:RCWidget?*",
+        "/load.php\?*only=styles*",
+        "/extensions/wikihow/*",
+        "/images/*",
+        "/skins/*"
+    ]
 
+#grouped_by_accepts = {}
+grouped_by_urls = {url_ctgr: set() for url_ctgr in url_categories}
+#  }}} Data Structure Definition # 
+
+#  Main Structure {{{ # 
 with open("../flows/wikihow-20220511-f.flow", "rb") as fl_f:
         #open("wikihow-urls.list", "w") as opt_f:
     flow_reader = io.FlowReader(fl_f)
@@ -62,34 +61,74 @@ with open("../flows/wikihow-20220511-f.flow", "rb") as fl_f:
                     new_attributes.add(k)
                 attributes[k].add(v)
 
-            accept_value = f.request.headers["accept"]
-            if accept_value not in grouped_by_accepts:
-                grouped_by_accepts[accept_value] = []
-            grouped_by_accepts[accept_value].append(f.request.path)
+            #accept_value = f.request.headers["accept"]
+            #if accept_value not in grouped_by_accepts:
+                #grouped_by_accepts[accept_value] = []
+            #grouped_by_accepts[accept_value].append(f.request.path)
+
+            #  URL Classification {{{ # 
+            url = f.request.path
+            if url.startswith("/x/"):
+                url_key = "/x/*"
+            elif url.startswith("/ev/"):
+                url_key = "/ev/*"
+            elif url.startswith("/load.php?"):
+                if "only=styles" in url:
+                    url_key = "/load.php\?*only=styles*"
+                else:
+                    url_key = "R /load.php?.\+$\(only=styles\)\@!"
+            elif url.startswith("/video/"):
+                url_key = "/video/*"
+            elif url=="/Special:SherlockController":
+                url_key = "/Special:SherlockController"
+            elif url.startswith("/Special:RCWidget?"):
+                url_key = "/Special:RCWidget?*"
+            elif url.startswith("/extensions/wikihow/"):
+                url_key = "/extensions/wikihow/*"
+            elif url.startswith("/images/"):
+                url_key = "/images/*"
+            elif url.startswith("/skins/"):
+                url_key = "/skins/*"
+            else:
+                url_key = "[[:others:]]"
+            #  }}} URL Classification # 
+
+            grouped_by_urls[url_key].add(f.request.headers.get("x-requested-with", None))
 
             if f.response is not None:
                 status_codes.add(f.response.status_code)
+#  }}} Main Structure # 
 
-print("\x1b[32mNew Attributes\x1b[0m")
-for k in new_attributes:
-    print(k)
-print()
+#  Output {{{ # 
+with open("attribute_statistics.list", "w") as f:
+    print("\x1b[32mNew Attributes\x1b[0m", file=f)
+    for k in new_attributes:
+        print(k, file=f)
+    print(file=f)
 
-for k in attributes:
-    if k=="cookie" or k=="referer":
-        continue
-    print("\x1b[32mAttribute {:}\x1b[0m".format(k))
-    for v in attributes[k]:
-        print(v)
-    print()
+    for k in attributes:
+        if k=="cookie" or k=="referer":
+            continue
+        print("\x1b[32mAttribute {:}\x1b[0m".format(k), file=f)
+        for v in attributes[k]:
+            print(v, file=f)
+        print(file=f)
 
-print("\x1b[32mStatus Codes\x1b[0m")
-for c in status_codes:
-    print(c)
+    print("\x1b[32mStatus Codes\x1b[0m", file=f)
+    for c in status_codes:
+        print(c, file=f)
 
-with open("accept-url.list", "w") as f:
-    for val, urls in grouped_by_accepts.items():
+#with open("accept-url.list", "w") as f:
+    #for val, urls in grouped_by_accepts.items():
+        #f.write("{:}:\n".format(val))
+        #for url in urls:
+            #f.write("{:}\n".format(url))
+        #f.write("\n")
+
+with open("url-x_requested_with.list", "w") as f:
+    for val, attrbs in grouped_by_urls.items():
         f.write("{:}:\n".format(val))
-        for url in urls:
-            f.write("{:}\n".format(url))
+        for attrb in attrbs:
+            f.write("{:}\n".format(attrb))
         f.write("\n")
+#  }}} Output # 
