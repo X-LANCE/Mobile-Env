6e16faa7d2c1a605d876ba426c9aa71cd00180c0
David Chang <zdy004007@126.com>
2022-08-12 22:48:31 +0800

    ver Aug12th
    debugged the replay program

--- apps/wikihow/mitmscripts/extract_urls.py
+++ apps/wikihow/mitmscripts/extract_urls.py
@@ -6,210 +6,26 @@ from mitmproxy import io
 
 import re
 import classify_url
-
-#  Data Structure Definition {{{ # 
-attributes = [
-        "upgrade-insecure-requests",
-        "user-agent",
-        "accept",
-        "x-requested-with",
-        "sec-fetch-size",
-        "sec-fetch-mode",
-        "sec-fetch-user",
-        "sec-fetch-dest",
-        "sec-fetch-site",
-        "accept-encoding",
-        "accept-language",
-        "cookie",
-        "content-type",
-        "content-length",
-        "range",
-        "origin",
-        "referer",
-    ]
-attributes = {attrb: set() for attrb in attributes}
-new_attributes = set()
-
-status_codes = set()
-
-url_categories = [
-        r"[[:others:]]",
-        r"/x/collect\?t={first,later}&*",
-        r"/x/zscsucgm\?",
-        r"/x/collect\?t={exit,amp}&*",
-        r"/x/amp-view\?*",
-        r"/ev/*",
-        r"R /load.php?.\+$\(only=styles\)\@!",
-        r"/video/*",
-        r"/Special:SherlockController",
-        r"/Special:RCWidget\?*",
-        r"/load.php\?*only=styles*",
-        r"/extensions/wikihow/*",
-        r"/images/*",
-        r"/skins/*"
-    ]
-
-#grouped_by_accepts = {}
-grouped_by_urls = {url_ctgr: set() for url_ctgr in url_categories}
-#grouped_by_methods = {}
-#grouped_by_sec_fectch_sites = {}
-#grouped_by_origin = {}
-grouped_by_referers = {True: set(), False: set()}
-#  }}} Data Structure Definition # 
+import os.path
+import load_response
 
 #  Main Structure {{{ # 
-with open("../flows/wikihow-20220511-f.flow", "rb") as fl_f:
-        #, open("pages/main.page.path.list", "w") as opt_f\
-        #:
-    flow_reader = io.FlowReader(fl_f)
-    counter = 0
-    for f in flow_reader.stream():
-        if isinstance(f, http.HTTPFlow) and f.request.pretty_host=="www.wikihow.com":
-            #opt_f.write(f.request.path + "\n")
-
-            #for k, v in f.request.headers.items():
-                #if k not in attributes:
-                    #attributes[k] = set()
-                    #new_attributes.add(k)
-                #attributes[k].add(v)
-
-            #accept_value = f.request.headers["accept"]
-            #if accept_value not in grouped_by_accepts:
-                #grouped_by_accepts[accept_value] = []
-            #grouped_by_accepts[accept_value].append(f.request.path)
-
-            url = f.request.path
-            url_key = classify_url.classify(url)
-
-            #if url_key==r"/Special:SherlockController":
-                #opt_f.write(f.request.content + b"\r")
-
-            #print(f.request.path_components)
-
-            #counter += 1
-            #if counter==10:
-                #break
-
-            #grouped_by_urls[url_key].add(f.request.headers.get("x-requested-with", None))
-            #grouped_by_urls[url_key].add(f.request.method)
-            #grouped_by_urls[url_key].add(f.request.headers.get("sec-fetch-mode", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("sec-fetch-dest", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("sec-fetch-site", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("accept-encoding", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("content-type", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("origin", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("upgrade-insecure-requests", None))
-            #grouped_by_urls[url_key].add("sec-fetch-size" in f.request.headers)
-            #grouped_by_urls[url_key].add(f.request.headers.get("sec-fetch-user", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("accept-language", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("range", None))
-            #grouped_by_urls[url_key].add(f.request.headers.get("user-agent", None))
-            grouped_by_urls[url_key].add("referer" in f.request.headers)
-
-            #if url_key=="/x/*":
-                #if f.request.method not in grouped_by_methods:
-                    #grouped_by_methods[f.request.method] = []
-                #grouped_by_methods[f.request.method].append(url)
-
-            #if url_key in {
-                        #r"[[:others:]]",
-                        ##r"/x/collect\?t={exit,amp}&*",
-                        ##r"/x/amp-view\?*",
-                        ##r"/images/*"
-                    #}:
-                #sec_fetch_site = f.request.headers.get("sec-fetch-site", None)
-                #if sec_fetch_site not in grouped_by_sec_fectch_sites:
-                    #grouped_by_sec_fectch_sites[sec_fetch_site] = []
-                #grouped_by_sec_fectch_sites[sec_fetch_site].append(url)
-
-            #if url_key in\
-                    #{ r"/x/collect\?t={exit,amp}&*"
-                    #, r"/x/amp-view\?*"
-                    #}:
-                #origin = f.request.headers.get("origin", None)
-                #if origin not in grouped_by_origin:
-                    #grouped_by_origin[origin] = []
-                #grouped_by_origin[origin].append(url)
-
-            if url_key==r"[[:others:]]":
-                grouped_by_referers["referer" in f.request.headers].add(url)
-
-            #if f.response is not None:
-                #status_codes.add(f.response.status_code)
+#with open("../flows/wikihow-20220812-debug.flow", "rb") as fl_f:
+    #flow_reader = io.FlowReader(fl_f)
+    #counter = 0
+    #for f in flow_reader.stream():
+        #if isinstance(f, http.HTTPFlow) and f.request.pretty_host=="www.wikihow.com":
+#
+            #url = f.request.path
+            #dump_name = url.replace("/", "%2f")
+            #if len(dump_name)>100:
+                #dump_name = dump_name[:100]
+#
+            #with open(os.path.join("pages/strange-index-page/", dump_name), "wb") as opt_f:
+                #opt_f.write(f.response.raw_content)
 #  }}} Main Structure # 
 
-#  Output {{{ # 
-#with open("attribute_statistics.list", "w") as f:
-    #print("\x1b[32mNew Attributes\x1b[0m", file=f)
-    #for k in new_attributes:
-        #print(k, file=f)
-    #print(file=f)
-#
-    #for k in attributes:
-        #if k=="cookie" or k=="referer":
-            #continue
-        #print("\x1b[32mAttribute {:}\x1b[0m".format(k), file=f)
-        #for v in attributes[k]:
-            #print(v, file=f)
-        #print(file=f)
-#
-    #print("\x1b[32mStatus Codes\x1b[0m", file=f)
-    #for c in status_codes:
-        #print(c, file=f)
+_, _, content = load_response.load_response("../../../../small-web-crawler/dumps/%2f?wh_an=1&amp=1")
 
-#with open("accept-url.list", "w") as f:
-    #for val, urls in grouped_by_accepts.items():
-        #f.write("{:}:\n".format(val))
-        #for url in urls:
-            #f.write("{:}\n".format(url))
-        #f.write("\n")
-
-#with open("method-url.list", "w") as f:
-    #for val, urls in grouped_by_methods.items():
-        #f.write("{:}:\n".format(val))
-        #for url in urls:
-            #f.write("{:}\n".format(url))
-        #f.write("\n")
-
-#with open("sec_fetch_site-url.list", "w") as f:
-    #for val, urls in grouped_by_sec_fectch_sites.items():
-        #f.write("{:}:\n".format(val))
-        #for url in urls:
-            #f.write("{:}\n".format(url))
-        #f.write("\n")
-
-#with open("origin-url.list", "w") as f:
-    #for val, urls in grouped_by_origin.items():
-        #f.write("{:}:\n".format(val))
-        #for url in urls:
-            #f.write("{:}\n".format(url))
-        #f.write("\n")
-
-with open("referer-url.list", "w") as f:
-    for val, urls in grouped_by_referers.items():
-        f.write("{:}:\n".format(val))
-        for url in urls:
-            f.write("{:}\n".format(url))
-        f.write("\n")
-
-#with open("url-x_requested_with.list", "w") as f:
-#with open("url-method.list", "w") as f:
-#with open("url-sec_fetch_mode.list", "w") as f:
-#with open("url-sec_fetch_dest.list", "w") as f:
-#with open("url-sec_fetch_site.list", "w") as f:
-#with open("url-accept_encoding.list", "w") as f:
-#with open("url-content_type.list", "w") as f:
-#with open("url-origin.list", "w") as f:
-#with open("url-upgrade_insecure_requests.list", "w") as f:
-#with open("url-sec_fetch_size.list", "w") as f:
-#with open("url-sec_fetch_user.list", "w") as f:
-#with open("url-accept_language.list", "w") as f:
-#with open("url-range.list", "w") as f:
-#with open("url-user_agent.list", "w") as f:
-with open("url-referer.list", "w") as f:
-    for val, attrbs in grouped_by_urls.items():
-        f.write("{:}:\n".format(val))
-        for attrb in attrbs:
-            f.write("{:}\n".format(attrb))
-        f.write("\n")
-#  }}} Output # 
+with open("pages/index3.html.gz", "wb") as f:
+    f.write(content)
--- apps/wikihow/mitmscripts/replay_url.py
+++ apps/wikihow/mitmscripts/replay_url.py
@@ -1,16 +1,21 @@
 from mitmproxy import http
+from mitmproxy import ctx
 import os.path
 import functools
 import classify_url
 import datetime
 import load_response
+import locale
+import gzip
 
 class Replayer:
     #  class `Replayer` {{{ # 
     def __init__(self, replay_path: str):
-        #self.replay_path = "mitmscripts/pages/test-mitmproxy/"
-        #self.replay_path = "../../../small-web-crawler/dumps"
+        locale.setlocale(locale.LC_TIME, "en_US.utf8")
+
         self.replay_path = replay_path
+        self.dateformat = "%a, %d %b %Y %H:%M:%S GMT"
+
         self.cache_index = 15090
 
     def request(self, flow: http.HTTPFlow):
@@ -27,7 +32,7 @@ class Replayer:
                 response_time = datetime.datetime.utcnow()
                 headers["retry-after"] = "0"
                 headers["accept-ranges"] = "bytes"
-                headers["date"] = response_time.strftime("%a, %d %b %Y %X GMT")
+                headers["date"] = response_time.strftime(self.dateformat)
                 headers["x-timer"] = "S{:.6f},VS0,VE0".format(response_time.timestamp())
                 headers["x-c"] = "cache-tyo{:d}-TYO,M".format(self.cache_index)
                 headers["x-content-type-options"] = "nosniff"
@@ -37,7 +42,7 @@ class Replayer:
                 headers["x-robots-tag"] = "noindex, nofollow"
 
                 flow.response = http.Response.make(204,
-                        headers=headers).refresh()
+                        headers=headers)
 
                 self.cache_index += 1
             else:
@@ -45,14 +50,44 @@ class Replayer:
                 if len(filename)>100:
                     filename = filename[:100]
                 filename = os.path.join(self.replay_path, filename)
+                #ctx.log.info("Requesting {:}".format(filename))
                 if not os.path.exists(filename):
+                    ctx.log.info("404: {:}".format(filename))
                     flow.response = http.Response.make(404)
                 else:
                     status_code, header, content = load_response.load_response(filename)
 
+                    if "content-encoding" in header\
+                            and header["content-encoding"]==b"gzip"\
+                            and len(content)>0:
+                        try:
+                            content = gzip.decompress(content)
+                        except Exception as e:
+                            ctx.log(str(e))
+
+                    response_time = datetime.datetime.utcnow()
+                    #if "date" in header:
+                        #header["date"] = response_time.strftime(self.dateformat)
+                    #if "last-modified" in header:
+                        #header["last-modified"] = (response_time - datetime.timedelta(days=1))\
+                                #.strftime(self.dateformat)
+                    #if "expires" in header:
+                        #header["expires"] = (response_time + datetime.timedelta(days=3650))\
+                                #.strftime(self.dateformat)
+                    if "x-timer" in header:
+                        header["x-timer"] = "S{:.6f},VS0,VE0".format(response_time.timestamp())
+                    if "x-c" in header:
+                        header["x-c"] = "cache-tyo{:d}-TYO,M".format(self.cache_index)
+
                     flow.response = http.Response.make(status_code,
                             content=content,
-                            headers=header).refresh()
+                            headers=header)
+                    flow.response.refresh()
+                    #ctx.log.info("WARN: {:}".format(content==flow.response.content))
+
+                    #self.cache_index += 1
+        else:
+            flow.response = http.Response.make(204)
     #  }}} class `Replayer` # 
 
 addons = [
