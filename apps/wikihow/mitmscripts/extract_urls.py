35c9f3222e4cbe0838a451437897f1208de48180
David Chang <zdy004007@126.com>
2022-08-19 13:34:15 +0800

    ver Aug19thv2
    preparing to compose the code to mock the search page

--- apps/wikihow/mitmscripts/extract_urls.py
+++ apps/wikihow/mitmscripts/extract_urls.py
@@ -10,22 +10,26 @@ import os.path
 import load_response
 
 #  Main Structure {{{ # 
-#with open("../flows/wikihow-20220812-debug.flow", "rb") as fl_f:
-    #flow_reader = io.FlowReader(fl_f)
-    #counter = 0
-    #for f in flow_reader.stream():
-        #if isinstance(f, http.HTTPFlow) and f.request.pretty_host=="www.wikihow.com":
-#
-            #url = f.request.path
-            #dump_name = url.replace("/", "%2f")
-            #if len(dump_name)>100:
-                #dump_name = dump_name[:100]
-#
-            #with open(os.path.join("pages/strange-index-page/", dump_name), "wb") as opt_f:
-                #opt_f.write(f.response.raw_content)
+with open("../flows/wikihow-20220511-f.flow", "rb") as fl_f:
+    flow_reader = io.FlowReader(fl_f)
+    counter = 0
+    for f in flow_reader.stream():
+        if isinstance(f, http.HTTPFlow) and f.request.pretty_host=="www.wikihow.com":
+
+            url = f.request.path
+            if url.startswith("/wikiHowTo?search="):
+                dump_name = url.replace("/", "%2f")
+                #if len(dump_name)>100:
+                    #dump_name = dump_name[:100]
+
+                with open(os.path.join("pages/search-pages/header", dump_name + ".txt"), "w") as opt_f:
+                    for k, v in f.response.headers.items():
+                        opt_f.write("{:}: {:}\n".format(k, v))
+                with open(os.path.join("pages/search-pages/html", dump_name + ".html.gz"), "wb") as opt_f:
+                    opt_f.write(f.response.raw_content)
 #  }}} Main Structure # 
 
-_, _, content = load_response.load_response("../../../../small-web-crawler/dumps/%2f?wh_an=1&amp=1")
-
-with open("pages/index3.html.gz", "wb") as f:
-    f.write(content)
+#_, _, content = load_response.load_response("../../../../small-web-crawler/dumps/%2f?wh_an=1&amp=1")
+#
+#with open("pages/index3.html.gz", "wb") as f:
+    #f.write(content)
