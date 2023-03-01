72bec5d4378c88cac552e819c63599a3f19809f3
zdy023 <zdy004007@126.com>
2023-03-01 19:54:37 +0800

    ver Mar1stv3
    updated revision and copyright claims required by the license
    deleted the file that shouldn't be distributed

--- tools/frida_setup.sh
+++ tools/frida_setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 # vimc: call SyntaxRange#Include('expect <<"EOF"', 'EOF', 'expect', 'NonText'):
+# Copyright 2023 SJTU X-Lance Lab
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Created by Danyang Zhang @X-Lance.
 
 export EMULATOR_PATH=${1:-$HOME/Android/Sdk/emulator}
 export AVD_NAME=${2:-Pixel_2_API_30_x64}
