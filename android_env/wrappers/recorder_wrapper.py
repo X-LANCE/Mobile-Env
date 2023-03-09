bda7fb85e2ab6c0b8f98eeb73f7e01f197ecddfb
zdy023 <zdy004007@126.com>
2023-03-09 10:57:02 +0800

    ver Mar9th
    updated RecorderWrapper to record the observation of the last step of
      the consecutive LIFTs

--- android_env/wrappers/recorder_wrapper.py
+++ android_env/wrappers/recorder_wrapper.py
@@ -11,13 +11,20 @@ import pickle as pkl
 
 # autosave at the reset time and episode end.
 
+def is_real_action(action: action_type.ActionType) -> bool:
+    return action==action_type.ActionType.TOUCH or action==action_type.ActionType.TEXT
+
 class RecorderWrapper(base_wrapper.BaseWrapper):
     #  class `RecorderWrapper` {{{ # 
     def __init__(self, env: AndroidEnv, dump_file: str):
         super(RecorderWrapper, self).__init__(env)
 
+        self._index: int = 0
         self.dump_file: str = os.path.splitext(dump_file)[0]
+
         self.prev_type: action_type.ActionType = action_type.ActionType.LIFT
+        self._buffer: Dict[str, Any] = {}
+        self.is_valid: bool = False
 
         #self.trajectories:\
             #List[List[Dict[str, Any]]]\
@@ -31,27 +38,43 @@ class RecorderWrapper(base_wrapper.BaseWrapper):
         timestep = self._env.step(action)
 
         current_type = action["action_type"].item()
-        if current_type!=action_type.ActionType.REPEAT and\
-                    not (current_type==action_type.ActionType.LIFT\
-                        and self.prev_type==action_type.ActionType.LIFT) or\
-                timestep.reward>0:
-            record = {}
-            record["action_type"] = current_type
-            if current_type==action_type.ActionType.TEXT:
-                record["input_token"] = action["input_token"].item()
-            elif current_type==action_type.ActionType.TOUCH:
-                record["touch_position"] = action["touch_position"]
-            record["reward"] = timestep.reward
-            instruction = self._env.task_instructions()
-            if len(instruction)>0:
-                record["instruction"] = instruction
-            record["observation"] = timestep.observation["pixels"]
-            record["view_hierarchy"] = lxml.etree.tostring(
-                    timestep.observation["view_hierarchy"],
-                    encoding="unicode") if timestep.observation["view_hierarchy"]\
-                            is not None else None
-            record["orientation"] = np.argmax(timestep.observation["orientation"])
-            self.current_trajectory.append(record)
+
+        # late judge
+        self.is_valid = self.is_valid\
+                     or ( self.prev_type==action_type.ActionType.LIFT
+                      and is_real_action(current_type)
+                        )
+
+        # dump the buffer
+        if self.is_valid:
+            self.current_trajectory.append(self._buffer)
+
+        # early judge
+        self.is_valid = current_type!=action_type.ActionType.REPEAT\
+                    and not ( current_type==action_type.ActionType.LIFT
+                          and self.prev_type==action_type.ActionType.LIFT
+                            )\
+                     or timestep.reward>0
+
+        record = {}
+        record["action_type"] = current_type
+        if current_type==action_type.ActionType.TEXT:
+            record["input_token"] = action["input_token"].item()
+        elif current_type==action_type.ActionType.TOUCH:
+            record["touch_position"] = action["touch_position"]
+        record["reward"] = timestep.reward
+        instruction = self._env.task_instructions()
+        if len(instruction)>0:
+            record["instruction"] = instruction
+        record["observation"] = timestep.observation["pixels"]
+        record["view_hierarchy"] = lxml.etree.tostring(
+                timestep.observation["view_hierarchy"],
+                encoding="unicode") if timestep.observation["view_hierarchy"]\
+                        is not None else None
+        record["orientation"] = np.argmax(timestep.observation["orientation"])
+        #self.current_trajectory.append(record)
+        self._buffer = record
+
         self.prev_type = current_type
 
         if timestep.last():
@@ -63,6 +86,9 @@ class RecorderWrapper(base_wrapper.BaseWrapper):
 
     def _process_timestep(self, timestep: dm_env.TimeStep) -> dm_env.TimeStep:
         if timestep.first():
+            self.is_valid = True
+            self.prev_type = action_type.ActionType.LIFT
+
             record = {}
             record["task_id"] = self._env.task_id
             record["task"] = self._env.task_name
@@ -72,7 +98,8 @@ class RecorderWrapper(base_wrapper.BaseWrapper):
                     encoding="unicode") if timestep.observation["view_hierarchy"]\
                             is not None else None
             record["orientation"] = np.argmax(timestep.observation["orientation"])
-            self.current_trajectory.append(record)
+            #self.current_trajectory.append(record)
+            self._buffer = record
         return timestep
 
     def _reset_state(self):
@@ -84,9 +111,14 @@ class RecorderWrapper(base_wrapper.BaseWrapper):
 
     def _save(self):
         #  function `_save` {{{ # 
+        if len(self._buffer)>0:
+            self.current_trajectory.append(self._buffer)
         if len(self.current_trajectory)>1:
-            with open(self.dump_file + ".pkl", "ab") as f:
+            with open(self.dump_file + ".{:d}.pkl".format(self._index), "ab") as f:
                 pkl.dump(self.current_trajectory, f)
+
         self.current_trajectory = []
+        self._buffer = {}
+        self._index += 1
         #  }}} function `_save` # 
     #  }}} class `RecorderWrapper` # 
