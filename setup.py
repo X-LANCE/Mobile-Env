c3b44c7f6b36eb69ce504fd24b4c04114ecdbbab
zdy023 <zdy004007@126.com>
2023-06-30 19:57:01 +0800

    ver Jun30thv3
    updated versin number

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ class _BuildPy(build_py):
 
 setup(
     name='mobile-env-rl',
-    version='1.1.0',
+    version='2.0',
     description='Mobile-Env: A Universal Platform for Training and Evaluation of Mobile Interaction',
     long_description=description,
     author='Danyang Zhang @X-Lance',
