5f48b4581f0f2c355d87413311ffab8e80d3fccc
David Chang <zdy004007@126.com>
2023-09-05 23:15:28 +0800

    ver Sep5th
    working on RemoteSimulator

--- android_env/components/simulators/remote/remote_simulator.py
+++ android_env/components/simulators/remote/remote_simulator.py
@@ -18,17 +18,32 @@ from android_env.components.simulators import base_simulator
 from android_env.components.adb_controller import AdbController
 from android_env.components.log_stream import LogStream
 
-#import requests
+import requests
 
 from typing import Dict, List
 from typing import Optional
 import numpy as np
 
+from absl import logging
+
+# ZDY_COMMENT: adb_root and frida_server arguments are ignored here
+# they should be configured directly on launching the daemon on server
+
 class RemoteSimulator(base_simulator.BaseSimulator):
     #  class RemoteSimulator {{{ # 
+    """
+    Protocol:
+    - without payloads:
+      + launch
+      + start
+      + close
+    """
+
     def __init__( self
                 , address: str
                 , port: int
+                , timeout: float = 5.
+                , retry: int = 3
                 , **kwargs
                 ):
         #  method __init__ {{{ # 
@@ -36,14 +51,38 @@ class RemoteSimulator(base_simulator.BaseSimulator):
 
         self._address: str = address
         self._port: int = port
+        self._url_base = "http://{:}:{:}/".format(self._address, self._port)
+
+        self._timeout: float = 5.
+        self._retry: int = 3
+
+        self._session: Optional[requests.Session] = None
         #  }}} method __init__ # 
+
+    def _get_response(self, action: str, args: Optional[Dict[str, str]] = None) -> requests.Response:
+        #  method _get_response {{{ # 
+        for i in range(self._retry):
+            response: requests.Response =\
+                    self._session.post( self._url_base + action
+                                      , json=args
+                                      , timeout=self._timeout
+                                      )
+            if response.status_code==200:
+                return response
+            logging.debug( "Remote Simulator Response Error %d: %d"
+                         , i, response.status_code
+                         )
+        raise ResponseError("Remote Simulator Response Error: {:d}"\
+                                .format(response.status_code)
+                           )
+        #  }}} method _get_response # 
 
     def _restart_impl(self):
         # TODO
         pass
     def _launch_impl(self):
-        # TODO
-        pass
+        self._session = requests.Session()
+        self._get_response("launch")
     def close(self):
         # TODO
         super(RemoteSimulator, self).close()
@@ -65,3 +104,7 @@ class RemoteSimulator(base_simulator.BaseSimulator):
         # TODO
         pass
     #  }}} class RemoteSimulator # 
+
+class ResponseError(Exception):
+    def __init__(self, description: str):
+        super(ResponseError, self).__init__(description)
