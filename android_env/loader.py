f17320b4fa5658c6133ade7d67f5fd02ea3a5b03
zdy023 <zdy004007@126.com>
2023-09-21 10:24:32 +0800

    ver Sep21stv3
    added load_remote function

--- android_env/loader.py
+++ android_env/loader.py
@@ -39,6 +39,7 @@ from android_env import environment
 from android_env.components import coordinator as coordinator_lib
 from android_env.components import task_manager as task_manager_lib
 from android_env.components.simulators.emulator import emulator_simulator
+from android_env.components.simulators.remote import remote_simulator
 from android_env.proto import task_pb2
 from android_env.components.tools.types import TextModel, IconModel
 from android_env.components.tools import naive_functions
@@ -202,6 +203,146 @@ def load( task_path: str
       adb_root=adb_root,
       frida_server=frida_server)
   #print("ZDY: AFTER Simulator Initialization")
+
+  if unify_vocabulary is not None:
+    with open(unify_vocabulary) as f:
+      vocabulary = list(map(str.strip, f))
+  else:
+    vocabulary = None
+
+  task_managers = list( map( functools.partial( task_manager_lib.TaskManager
+                                              , start_token_mark=start_token_mark
+                                              , non_start_token_mark=non_start_token_mark
+                                              , special_token_pattern=special_token_pattern
+                                              , fix_vocabulary_to=vocabulary
+                                              , text_model=text_model
+                                              , icon_model=icon_model
+                                              )
+                           , task_list
+                           )
+                      )
+  coordinator = coordinator_lib.Coordinator( simulator, task_managers
+                                           , with_view_hierarchy=with_view_hierarchy
+                                           )
+
+  # Load environment.
+  return environment.AndroidEnv(coordinator=coordinator)
+
+def load_remote( task_path: str
+               , address: str
+               , port: int
+               , timeout: float = 5.
+               , launch_timeout: float = 2.
+               , retry: int = 3
+               , mitm_config: Optional[Dict[str, str]] = None
+               , start_token_mark: str = ""
+               , non_start_token_mark: str = "##"
+               , special_token_pattern: str = r"\[\w+\]"
+               , unify_vocabulary: Optional[str] = None
+               , text_model: TextModel = naive_functions
+               , icon_model: IconModel = naive_functions
+               , with_view_hierarchy: bool = False
+               ) -> environment.AndroidEnv:
+  """Loads an AndroidEnv instance.
+
+  Args:
+    task_path (str): Path to the task textproto file or the directory of task
+      textproto file set
+
+    address (str): IP address of remote daemon
+    port (int): listening port of remote daemone
+    timeout (float): timeout for regular commands in secondes
+    launch_timeout (float): timeout for commands `launch`, `start`, and
+      `close` in **minutes**
+    retry (int): retry times
+
+    mitm_config: An optional dict to config the launch options if an mitm proxy
+      is in need for a web-based app. The dict is expected to have a struture
+      like
+        {
+            "method": "syscert" | "frida" | "packpatch",
+            "address": str as the address for the mitm proxy server, default to
+              "127.0.0.1",
+            "port": str as the port for the mitm proxy server, default to
+              "8080",
+            "frida-server": an option for method "frida", to indicate the path
+              to the executable of frida-server on the android system, default
+              to "/data/local/tmp/frida-server",
+            "frida": an option for method "frida", to indicate the path to the
+              executable of frida on the local machine, default to "frida",
+            "frida-script": an option for method "frida", to indicate the
+              script used to hijack the target applicationa, default to
+              "frida-script.js",
+            "patch-suffix": an option for method "packpatch", to indicate the
+              suffix $suff with which the package name "$package.apk" to be
+              installed is replaced to "$package-$suff.apk", default to
+              "patched"
+        }
+
+    start_token_mark (str): the mark for the start subwords such as "Ġ"
+      (\\u0120) for GPT subword
+    non_start_token_mark (str): the mark for the non-start subwords such as
+      "##" for BERT subword
+    special_token_pattern (str): the pattern of the special non-printable
+      tokens such as "[CLS]" for BERT tokens
+    unify_vocabulary (Optional[str]): a file name to the vocabulary file in
+      which each line constitutes a token.
+
+    text_model (TextModel): text model to conduct text recognition on the
+      screen
+    icon_model (IconModel): icon model to conduct icon recognition on the
+      screen
+    with_view_hierarchy (bool): if the view hierarchy should be included in the
+      observation
+
+  Returns:
+    env: An AndroidEnv instance.
+  """
+
+  # Prepare task.
+  task_list = []
+  if os.path.isdir(task_path):
+    task_paths = list( map( functools.partial(os.path.join, task_path)
+                          , sorted(
+                              filter( lambda f: f.endswith(".textproto")
+                                    , os.listdir(task_path)
+                                    )
+                              )
+                          )
+                     )
+    task_directory = task_path
+  else:
+    task_paths = [task_path]
+    task_directory = os.path.dirname(task_path)
+  for t_p in task_paths:
+    task = task_pb2.Task()
+    with open(t_p, 'r') as proto_file:
+      text_format.Parse(proto_file.read(), task)
+    task_list.append(task)
+
+  if mitm_config is not None and mitm_config["method"]=="packpatch":
+    for t in task_list:
+      for st in t.setup_steps:
+        if st.HasField("adb_call") and\
+            st.adb_call.HasField("install_apk"):
+          apk_path = st.adb_call.install_apk.filesystem.path
+          main_name, extension = os.path.splitext(apk_path)
+          st.adb_call.install_apk.filesystem.path =\
+              "{:}-{:}{:}".format(main_name,
+                mitm_config.get("patch-suffix", "patched"),
+                extension)
+
+  # transform the paths in task definitions
+  for t in task_list:
+    fix_path(t, task_directory)
+
+  # Create simulator.
+  simulator = remote_simulator.RemoteSimulator( address=address
+                                              , port=port
+                                              , timeout=timeout
+                                              , launch_timeout=launch_timeout
+                                              , retry=retry
+                                              )
 
   if unify_vocabulary is not None:
     with open(unify_vocabulary) as f:
