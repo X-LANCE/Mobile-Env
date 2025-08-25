4849e5d8e0227165ec47c763d41af3f6c425b661
David Chang <zdy004007@126.com>
2025-08-25 22:43:24 +0800

    ver Aug25th
    SimpleTextLLMAgent and NaiveVLMAgent

--- android_env/templates/agents.py
+++ android_env/templates/agents.py
@@ -24,6 +24,12 @@ from android_env.templates.llm_caller import ResponseError as LC_ResponseError
 import openai
 import numpy as np
 
+from android_env.wrappers import VhIoWrapper
+#from copy import deepcopy
+from android_env.templates.utils import ( convert_vh_to_html_list
+                                        , convert_raw_img_to_jpeg_base64
+                                        )
+
 import logging
 
 logger = logging.getLogger("mobile_env.agents")
@@ -37,7 +43,7 @@ class WikiHowAgent(abc.ABC, Generic[Obs, Act, Reset]):
     def __init__(self):
         #  method __init__ {{{ # 
         self._instruction_history: List[str] = []
-        self._action_history: List[str] = []
+        self._action_history: List[Act] = []
         self._total_reward: float = 0.
         #  }}} method __init__ # 
 
@@ -179,7 +185,7 @@ class NaiveTextLLMAgent(WikiHowAgent[str, str, None]):
 
     def _get_action( self
                    , observation: Optional[str] = None
-                   , last_reward: Optional[str] = None
+                   , last_reward: Optional[float] = None
                    , **kwargs
                    ) -> Optional[str]:
         #  method _get_action {{{ # 
@@ -213,14 +219,234 @@ class NaiveTextLLMAgent(WikiHowAgent[str, str, None]):
 
 class SimpleTextLLMAgent(WikiHowAgent[Dict[str, np.ndarray], Dict[str, np.ndarray], None]):
     #  class SimpleTextLLMAgent {{{ # 
-    # TODO
-    pass
+    """
+    Simple Text-LLM-based agent working with `android_env.wrappers.VhIoWrapper`
+    """
+
+    def __init__( self
+                , prompt_template: TemplateGroup
+                , action_parser: Callable[[str], Dict[str, np.ndarray]]
+                , llm_caller_function: Optional[Callable[[PromptGroupT, int, float], str]] = None
+                , model_name: Optional[str] = None
+                , api_key: Optional[str] = None
+                , base_url: Optional[str] = None
+                , max_tokens: int = 100
+                , temperature: float = .1
+                ):
+        #  method __init__ {{{ # 
+        """
+        Args:
+            prompt_template (TemplateGroup): prompt template for the LLM. the
+              slots to be filled include
+              * history_instructions: '\n'-joined instructions for the early
+                stages
+              * current_instruction: the instruction for the current stage
+              * screen: screen representation in str
+              * action_history: '\n'-joined history action list
+            action_parser (Callable[[str], Dict[str, np.ndarray]]): function to
+              parse action from the llm response
+
+            llm_caller_function (Optional[Callable[[PromptGroupT, int, float], str]]):
+              function to invoke the LLM. the parameters are
+              * PromptGroupT: the input message
+              * int: the max number of tokens to generate
+              * float: generation temperature
+              if given, `api_key` and `base_url` arguments will be ignored. if
+              not given, an instance of
+              `android_env.templates.llm_caller.OpenAIProxy` will be built
+              accroding to `api_key` and `base_url`
+            model_name (Optional[str]): if `llm_caller_function`, this argument
+              will be used to build a LLM caller. model name to invoke
+            api_key (Optional[str]): if `llm_caller_function`, this argument
+              will be used to build a LLM caller.
+            base_url (Optional[str]): if `llm_caller_function`, this argument
+              will be used to build a LLM caller. keep None if change of base
+              url is not needed.
+
+            max_tokens (int): max number of tokens to generate
+            temperature (float): generation temperature
+        """
+
+        super(SimpleTextLLMAgent, self).__init__()
+
+        self._prompt_template: TemplateGroup = prompt_template
+        self._parse_action: Callable[[str], Dict[str, np.ndarray]] = action_parser
+
+        if llm_caller_function is not None:
+            self._llm_caller: Callable[[PromptGroupT, int, float], str] = llm_caller_function
+        else:
+            assert model_name is not None and api_key is not None\
+                 , "`model_name` and `api_key` are required to build OpenAIProxy when `llm_caller_function` argument is missed"
+            self._llm_caller: Callable[[PromptGroupT, int, float], str] =\
+                    OpenAIProxy(api_key, base_url, model_name)
+
+        self._max_tokens: int = max_tokens
+        self._temperature: float = temperature
+        #  }}} method __init__ # 
+
+    @property
+    def _default_empty_action(self) -> Dict[str, np.ndarray]:
+        return {"action_type": VhIoWrapper.ActionType.NOTHING}
+
+    def _get_action( self
+                   , observation: Optional[Dict[str, np.ndarray]] = None
+                   , last_reward: Optional[float] = None
+                   , **kwargs
+                   ) -> Optional[str]:
+        #  method _get_action {{{ # 
+        if observation is None or observation.get("view_hierarchy", None) is None:
+            return None
+
+        screen_representation: str = convert_vh_to_html_list(observation["view_hierarchy"])
+
+        message: PromptGroupT =\
+                self._prompt_template.safe_substitute(
+                        text_mapping={ "history_instructions":
+                                        "\n".join(self._instruction_history[:-1])
+                                     , "current_instruction":
+                                        self._instruction_history[-1] if len(self._instruction_history)>0 else ""
+                                     , "screen": screen_representation
+                                     , "action_history": "\n".join(self._action_history)
+                                     }
+                      )
+        try:
+            response: str = self._llm_caller(message, self._max_tokens, self._temperature)
+        except (openai.BadRequestError, LC_ResponseError, openai.InternalServerError):
+            logger.exception("LLM Request Error!")
+            action = None
+
+        try:
+            action: Dict[str, np.ndarray] = self._parse_action(response)
+        except:
+            logger.exception("Action Parsing Error!")
+            action: Dict[str, np.ndarray] = self._default_empty_action
+
+        return action
+        #  }}} method _get_action # 
     #  }}} class SimpleTextLLMAgent # 
 
 class NaiveVLMAgent(WikiHowAgent[np.ndarray, str, None]):
     #  class NaiveVLMAgent {{{ # 
-    # TODO
-    pass
+    """
+    Naive VLM-based agent accepting screen observation in image and returning
+    action in str.
+    """
+
+    def __init__( self
+                , prompt_template: TemplateGroup
+                , action_parser: Callable[[str], str]
+                , empty_action: str = "DO_NOTHING()"
+                , llm_caller_function: Optional[Callable[[PromptGroupT, int, float], str]] = None
+                , model_name: Optional[str] = None
+                , api_key: Optional[str] = None
+                , base_url: Optional[str] = None
+                , max_tokens: int = 100
+                , temperature: float = .1
+                ):
+        #  method __init__ {{{ # 
+        """
+        Args:
+            prompt_template (TemplateGroup): prompt template for the LLM. the
+              text slots to be filled include
+              * history_instructions: '\n'-joined instructions for the early
+                stages
+              * current_instruction: the instruction for the current stage
+              * action_history: '\n'-joined history action list
+              the image slot to be filled include
+              * screen: screen representation
+            action_parser (Callable[[str], str]): function to parse action from
+              the llm response
+
+            empty_action (str): the format of the empty action. related to the
+              specific prompt (or model)
+
+            llm_caller_function (Optional[Callable[[PromptGroupT, int, float], str]]):
+              function to invoke the LLM. the parameters are
+              * PromptGroupT: the input message
+              * int: the max number of tokens to generate
+              * float: generation temperature
+              if given, `api_key` and `base_url` arguments will be ignored. if
+              not given, an instance of
+              `android_env.templates.llm_caller.OpenAIProxy` will be built
+              accroding to `api_key` and `base_url`
+            model_name (Optional[str]): if `llm_caller_function`, this argument
+              will be used to build a LLM caller. model name to invoke
+            api_key (Optional[str]): if `llm_caller_function`, this argument
+              will be used to build a LLM caller.
+            base_url (Optional[str]): if `llm_caller_function`, this argument
+              will be used to build a LLM caller. keep None if change of base
+              url is not needed.
+
+            max_tokens (int): max number of tokens to generate
+            temperature (float): generation temperature
+        """
+
+        super(NaiveVLMAgent, self).__init__()
+
+        self._prompt_template: TemplateGroup = prompt_template
+        self._parse_action: Callable[[str], str] = action_parser
+
+        if llm_caller_function is not None:
+            self._llm_caller: Callable[[PromptGroupT, int, float], str] = llm_caller_function
+        else:
+            assert model_name is not None and api_key is not None\
+                 , "`model_name` and `api_key` are required to build OpenAIProxy when `llm_caller_function` argument is missed"
+            self._llm_caller: Callable[[PromptGroupT, int, float], str] =\
+                    OpenAIProxy(api_key, base_url, model_name)
+
+        self._max_tokens: int = max_tokens
+        self._temperature: float = temperature
+
+        self._empty_action: str = empty_action
+        #  }}} method __init__ # 
+
+    @property
+    def _default_empty_action(self) -> str:
+        return self._empty_action
+
+    def _get_action( self
+                   , observation: Optional[np.ndarray] = None
+                   , last_reward: Optional[float] = None
+                   , **kwargs
+                   ) -> Optional[str]:
+        #  method _get_action {{{ # 
+        """
+        Args:
+            observation (Optional[np.ndarray]): the image representation of the
+              screen. is expected to be have the shape like (H, W, 3) with
+              channels RGB.
+            last_reward (Optional[float]): the reward of the last step
+
+        Returns:
+            str: the action representation
+        """
+
+        if observation is None:
+            return None
+        
+        message: PromptGroupT =\
+                self._prompt_template.safe_substitute(
+                        text_mapping={ "history_instructions":
+                                        "\n".join(self._instruction_history[:-1])
+                                     , "current_instruction":
+                                        self._instruction_history[-1] if len(self._instruction_history)>0 else ""
+                                     , "action_history": "\n".join(self._action_history)
+                                     }
+                      , img_mapping={"screen": convert_raw_img_to_jpeg_base64(observation)}
+                      )
+        try:
+            response: str = self._llm_caller(message, self._max_tokens, self._temperature)
+        except (openai.BadRequestError, LC_ResponseError, openai.InternalServerError):
+            logger.exception("LLM Request Error!")
+            action = None
+
+        try:
+            action: str = self._parse_action(response)
+        except:
+            logger.exception("Action Parsing Error!")
+            action: str = self._default_empty_action
+        return action
+        #  }}} method _get_action # 
     #  }}} class NaiveVLMAgent # 
 
 class SimpleVLMAgent(WikiHowAgent[Dict[str, np.ndarray], Dict[str, np.ndarray], None]):
--- android_env/templates/utils.py
+++ android_env/templates/utils.py
@@ -14,11 +14,131 @@
 # 
 # Created by Danyang Zhang @X-Lance
 
-from typing import Union
-from typing import Dict
+from typing import Union, Callable, Literal
+from typing import Dict, List
+import numpy as np
+from lxml.etree import _Element
+from lxml.html import HtmlElement
+import lxml.html
+import lxml.etree
+from android_env.wrappers.vh_io_wrapper import filter_elements
 
-def parse_vh_vlm_action_from_action_markup(response: str) -> Union[str, Dict[str, np.ndarray]]:
+def parse_vh_vlm_action_from_action_markup( response: str
+                                          , parse_for: Literal["text", "vh_env", "tap_env"] = "text"
+                                          ) -> Union[str, Dict[str, np.ndarray]]:
     #  function parse_vh_vlm_action_from_action_markup {{{ # 
+    # NOTE: add a special field in actions with element ids for element
+    # representations
     # TODO
     pass
     #  }}} function parse_vh_vlm_action_from_action_markup # 
+
+def convert_action_object_to_history(action_object: Dict[str, np.ndarray]) -> str:
+    #  function convert_action_object_to_history {{{ # 
+    # TODO
+    pass
+    #  }}} function convert_action_object_to_history # 
+
+def convert_vh_node_to_html(node: _Element) -> HtmlElement:
+    #  function convert_vh_node_to_html {{{ # 
+    """
+    Converts one leaf node in android view hierarchy to html element. Will
+    convert the class, text, resource-id, and content-desc properties.
+
+    Args:
+        node (lxml.etree.Element): leaf node from an android view hierarchy
+
+    Returns:
+        lxml.html.Element: the converted html element. usually is p, button,
+          img, input, or div.
+    """
+
+    attribute_dict: Dict[str, str] = {}
+
+    # convert resource-id
+    resource_id: str = node.get("resource-id")
+    if len(resource_id)>0:
+        resource_identifyers = resource_id.rsplit("/", maxsplit=1)
+        #assert len(resource_identifyers)==2
+        attribute_dict["class"] = " ".join(resource_identifyers[-1].split("_"))
+
+    # convert content-desc
+    content_desc: str = node.get("content-desc")
+    if len(content_desc)>0:
+        attribute_dict["alt"] = content_desc
+
+    # convert text
+    text: str = node.get("text")
+
+    # convert class
+    vh_class_name: str = node.get("class")
+    if vh_class_name.endswith("TextView"):
+        html_element = lxml.html.Element( "p"
+                                        , attribute_dict
+                                        )
+        if len(text)>0:
+            html_element.text = text
+    elif vh_class_name.endswith("Button")\
+            or vh_class_name.endswith("MenuItemView"):
+        html_element = lxml.html.Element( "button"
+                                        , attribute_dict
+                                        )
+        if len(text)>0:
+            html_element.text = text
+    elif vh_class_name.endswith("ImageView")\
+            or vh_class_name.endswith("IconView")\
+            or vh_class_name.endswith("Image"):
+        if len(text)>0:
+            if "alt" in attribute_dict:
+                attribute_dict["alt"] += ": " + text
+            else:
+                attribute_dict["alt"] = text
+        html_element = lxml.html.Element( "img"
+                                        , attribute_dict
+                                        )
+    elif vh_class_name.endswith("EditText"):
+        if len(text)>0:
+            attribute_dict["value"] = text
+        attribute_dict["type"] = "text"
+        html_element = lxml.html.Element( "input"
+                                        , attribute_dict
+                                        )
+    else:
+        html_element = lxml.html.Element( "div"
+                                        , attribute_dict
+                                        )
+        if len(text)>0:
+            html_element.text = text
+
+    return html_element
+    #  }}} function convert_vh_node_to_html # 
+
+def escape_newlines_to_html(html: str) -> str:
+    return html.strip().replace("\n", "&#10;").replace("\r", "&#13;")
+
+def convert_vh_to_html_list(node: _Element) -> str:
+    #  function convert_vh_to_html_list {{{ # 
+    """
+    Args:
+        node (_Element): the VH tree to convert
+
+    Returns:
+        str: converted HTML list, one element on one line
+    """
+
+    node_list: List[_Element] = filter_elements(node)[0]
+    result_list: List[str] = []
+
+    annotate_id: Callable[[HtmlElement, int], None] = lambda elm, i: elm.set("id", str(i))
+    def annotate_clickable(elm: HtmlElement, n: _Element):
+        elm.set("clickable", n.get("clickable"))
+        elm.set("long-clickable", n.get("long-clickable"))
+
+    for i, n in enumerate(node_list):
+        html_element: lxml.html.Element = convert_vh_node_to_html(n)
+        annotate_id(html_element, i)
+        annotate_clickable(html_element, n)
+        result_list.append(escape_newlines_to_html(lxml.html.tostring(html_element, pretty_print=True, encoding="unicode")))
+
+    return "\n".join(result_list)
+    #  }}} function convert_vh_to_html_list # 
